76a57acffdc3a9e37dbc325e86679fe2e291a4b8
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-02-15 21:22:59 +0100

    Animace pro majáles

---
+++ eng/musicfestivalsumperk/majalesShort.js
@@ -0,0 +1,7 @@
+let mShort = bodymovin.loadAnimation({
+    container: document.getElementById('mShort'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'majalesShort.json'
+})
---
+++ sumperskymajales/majalesLong.js
@@ -0,0 +1,7 @@
+let mLong = bodymovin.loadAnimation({
+    container: document.getElementById('mLong'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'majalesLong.json'
+})
