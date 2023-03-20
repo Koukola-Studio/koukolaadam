2c8810fa37c43f240d1a6a5debbceb2c54d79101
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-03-20 22:02:55 +0100

    Přidání projektu

---
+++ eng/scripts/slusLogo.js
@@ -0,0 +1,7 @@
+let slusLogoDesign = bodymovin.loadAnimation({
+    container: document.getElementById('slusLogo'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'slusLogo.json'
+})
