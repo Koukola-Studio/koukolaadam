4e4cdfc0102c3e7ab8b8a4dc2232435d8cf923e4
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-07-20 19:00:38 +0200

    Add files via upload

---
+++ scripts/edudigital.js
@@ -0,0 +1,7 @@
+let edu = bodymovin.loadAnimation({
+    container: document.getElementById('edudigital'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'edudigital.json'
+})
